ee18901688ae886717db90db2d18447bc6c130e7
John Sasser <cw.john.sasser@gmail.com>
2020-02-15 17:09:50 -0500

    final version: completed

--- index.js
+++ index.js
@@ -4,44 +4,58 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 // questions for the inquirer
 const questions = require("./lib/questions");
-// require classes
+// empty string for the generated card data to be loaded to the html;
+let html = "";
+// require classes;
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const teamArr = [];
-
-// prompt with initial questions
-// depending on the choice create the teamArr
-// put the values from the prompts into the classes
 function createEmployee() {
 	inquirer
 		.prompt(questions.empQuestions)
 		.then(answers => {
-			switch (answers.title) {
-				case "Intern":
-					inquirer.prompt(questions.internQuestion).then(internAnswers => {
-						const newAnswers = { ...answers, internAnswers };
-						teamArr.push(newAnswers);
-
-						restartInquirer();
-					});
-					break;
+			switch (answers.role) {
 				case "Engineer":
 					inquirer.prompt(questions.engQuestion).then(engineerAnswers => {
-						const newAnswers = { ...answers, engineerAnswers };
-						teamArr.push(newAnswers);
+						const engineerData = new Engineer(
+							answers.name,
+							answers.id,
+							answers.email,
+							engineerAnswers.github
+						);
+						readEngFile(engineerData);
 
 						restartInquirer();
 					});
 					break;
 				case "Manager":
-					inquirer.prompt(questions.mgmtQuestion).then(managerAnswers => {
-						const newAnswers = { ...answers, managerAnswers };
-						teamArr.push(newAnswers);
+					inquirer.prompt(questions.mgmtQuestion).then(async managerAnswers => {
+						const managerData = await new Manager(
+							answers.name,
+							answers.id,
+							answers.email,
+							managerAnswers.officeNumber
+						);
+						readMgnFile(answers, managerAnswers);
 
 						restartInquirer();
 					});
+					break;
+				case "Intern":
+					inquirer
+						.prompt(questions.internQuestion)
+						.then(async internAnswers => {
+							const internData = await new Intern(
+								answers.name,
+								answers.id,
+								answers.email,
+								internAnswers.internSchool
+							);
+							readInternFile(answers, internAnswers);
+
+							restartInquirer();
+						});
 					break;
 			}
 		})
@@ -52,15 +66,75 @@ function createEmployee() {
 
 function restartInquirer() {
 	inquirer.prompt(questions.newQuestion).then(answer => {
-		switch (answer.newQuestion) {
+		switch (answer.role) {
 			case "YES!!!":
 				createEmployee();
 				break;
+
 			case "NO!":
-				generateHTML();
+				createHTML();
 				break;
 		}
 	});
 }
+
+// working
+function readEngFile(engineerData) {
+	// console.log(engineerData);
+	// data is my html string,
+	fs.readFile("./html/engineer.html", "utf8", function(error, data) {
+		// console.log(engineerData.name);
+		const newData = data
+			.replace("Ename", engineerData.name)
+			.replace("Eid", engineerData.id)
+			.replace("Eemail", engineerData.email)
+			.replace("Egighub", engineerData.github);
+
+		// read .html for all class values and the combine them before writing the file.
+		html += newData;
+		// console.log(html);
+	});
+}
+// not working
+function readMgnFile(managerData) {
+	// data is my html string,
+	fs.readFile("./html/manager.html", "utf8", function(error, data) {
+		const newData = data
+			.replace("Mname", managerData.name)
+			.replace("Micon", managerData.id)
+			.replace("Memail", managerData.email)
+			.replace("Mgithub", managerData.officeNumber);
+
+		// read .html for all class values and the combine them before writing the file.
+		html += newData;
+	});
+}
+// not working
+function readInternFile(internData) {
+	// data is my html string,
+	fs.readFile("./html/intern.html", "utf8", function(error, data) {
+		const newData = data
+			.replace("Iname", internData.name)
+			.replace("Iicon", internData.id)
+			.replace("Iemail", internData.email)
+			.replace("Igithub", internData.internSchool);
+
+		// read .html for all class values and the combine them before writing the file.
+		html += newData;
+	});
+}
+
+function createHTML() {
+	fs.readFile("./html/main.html", "utf8", (err, data) => {
+		const newData = data.replace("{{html}}", html);
+
+		fs.writeFile("./output/index.html", newData, "utf8", err => {
+			if (err) return console.log(err);
+		});
+		console.log(".html created");
+	});
+}
+
+module.exports = {};
 
 createEmployee();
